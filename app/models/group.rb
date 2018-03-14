7e3bcbff5a00c0c2d6750fcbdfdb2bde450fda53
Yungchih Chen <yungchih_chen@yahoo.com>
2018-03-14 00:21:11 -0400

    refactor group organizers

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   has_many :memberships
   has_many :members, through: :memberships, source: :user
+
+  def group_organizers
+    self.memberships.where(role_id: Role.find_by_name('Organizer').id).map{|m| m.user.full_name}.join(",")
+  end
 end
